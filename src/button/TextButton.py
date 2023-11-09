710a1cb5dbfb2fb9fce6d83c8f13a75d47c47407
femifacia <femi.facia@epitech.eu>
2023-11-09 06:56:49 +0100

    [ADD] Scene and button skeleton

---
+++ src/button/TextButton.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class TextButton:
+
+    def __init__(self, x = 100, y = 100, text="Hello", font_size=16, font_system="Arial", font_path = "") -> None:
+
+
+        pass
---
+++ src/scene/scene.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+class Scene:
+
+    def __init__(self) -> None:
+        pass
